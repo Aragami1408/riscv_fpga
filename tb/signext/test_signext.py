ffae4ad34388093e05cb9585e31b72b6335dc538
Cao Minh Vu <vucaominh1408@gmail.com>
2026-06-25 22:50:07 +0700

    signext for s-type instruction test passed

--- tb/signext/test_signext.py
+++ tb/signext/test_signext.py
@@ -34,3 +34,31 @@ async def signext_i_type_test(dut):
     await Timer(1, unit="ns") # advance to calculate
     assert dut.immediate.value == "11111111111111111111111111010110"
     assert int(dut.immediate.value) - (1 << 32) == -42
+
+@cocotb.test()
+async def signext_s_type_test(dut):
+    # 100 randomised tests
+    for _ in range(100):
+        # TEST POSITIVE IMM
+        await Timer(100, unit="ns")
+        imm = random.randint(0,0b01111111111)
+        imm_11_5 = imm >> 5
+        imm_4_0 = imm & 0b000000011111
+        raw_data = (imm_11_5 << 18) | (imm_4_0) # the 25 bits of data
+        source = 0b01
+        dut.raw_src.value = raw_data
+        dut.imm_source.value = source
+        await Timer(1, unit="ns")
+        assert int(dut.immediate.value) == imm
+
+        # TEST NEGATIVE IMM
+        # Ger a random 12 bits UNIT and gets its base 10 neg value by - (1 << 12)
+        imm = random.randint(0b100000000000,0b111111111111) - (1 << 12)
+        imm_11_5 = imm >> 5
+        imm_4_0 = imm & 0b000000011111
+        raw_data = (imm_11_5 << 18) | (imm_4_0) # the 25 bits of data
+        source = 0b01
+        dut.raw_src.value = raw_data
+        dut.imm_source.value = source
+        await Timer(1, unit="ns")
+        assert int(dut.immediate.value) - (1 << 32) == imm
